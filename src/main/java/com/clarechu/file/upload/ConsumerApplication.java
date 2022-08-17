6ad7e0d32a5f888b918296a8b2faf5ff820e03ca
clarechu <1062186165@qq.com>
2022-08-17 11:04:25 +0800

    删除swagger的依赖问题

--- src/main/java/com/clarechu/file/upload/ConsumerApplication.java
+++ src/main/java/com/clarechu/file/upload/ConsumerApplication.java
@@ -1,9 +1,7 @@
 package com.clarechu.file.upload;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@EnableSwagger2Doc
 @SpringBootApplication
 public class ConsumerApplication {
 
